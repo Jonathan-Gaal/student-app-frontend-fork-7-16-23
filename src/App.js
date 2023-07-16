9dfcc1ad3e6f6c44298d80a905e656a66dc7a14f
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-07-16 16:25:06 -0400

    css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import StudentList from './components/StudentList/StudentList';
+import { useEffect, useState } from "react";
+import "./App.css";
+import StudentList from "./components/StudentList/StudentList";
 
 // TODO: Get this value from .env
-const API_URL = 'http://localhost:8888';
+const API_URL = "http://localhost:8888";
 
 function App() {
   const [studentData, setStudentData] = useState([]);
 
   useEffect(() => {
-    console.log('<App /> useEffect() fired');
+    console.log("<App /> useEffect() fired");
     async function fetchData() {
       // You can await here
       const response = await fetch(`${API_URL}/students`);
       const json = await response.json();
-      console.log('<App /> useEffect() fetched data', json);
+      console.log("<App /> useEffect() fetched data", json);
       const { data } = json;
       setStudentData(data);
     }
